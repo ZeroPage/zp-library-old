c4d4e4ec71b95722081c75996139b7ea0743c876
kameru <miri1208@naver.com>
2014-11-27 19:00:32 +0900

    forms.py changed

--- zp_library/forms.py
+++ zp_library/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 
 class BookForm(forms.Form):
-    name = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea)
+    ISBN = forms.CharField()
+    title = forms.CharField()
+    author = forms.CharField()
+    translator = forms.CharField()
+    publisher = forms.CharField()
+    publishedDate = forms.DateField()
+    description = forms.CharField(widget=forms.Textarea)
+    category = forms.CharField()
+    language = forms.CharField()
+    smallThumbnail = forms.CharField()
+    thumbnail = forms.CharField()
+    pageCount = forms.IntegerField()
+    bookCount = forms.IntegerField()
+    donor = forms.CharField()
 
     def action(self):
         # send email using the self.cleaned_data dictionary
