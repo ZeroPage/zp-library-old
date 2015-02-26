a7df986924cbcea5833f64db19a851aed04e8a56
Skywave <skywave@live.co.kr>
2015-02-27 07:52:49 +0900

    layout update

--- static/js/angular.js
+++ static/js/angular.js
@@ -1,7 +1,16 @@
 var app = angular.module('LibraryApp', ['ngMaterial']);
 
-app.controller('LibraryControl', function($scope, $timeout, $mdSidenav, $log) {
+app.controller('LibraryControl', function($scope, $timeout, $mdSidenav, $mdToast) {
     $scope.toggleLeft = function() {
         $mdSidenav('left').toggle();
+    };
+
+    $scope.showToast = function() {
+        $mdToast.show(
+            $mdToast.simple()
+                .content('NOTICE - Under Development!')
+                .action('OK')
+                .hideDelay(0)
+        );
     };
 });
