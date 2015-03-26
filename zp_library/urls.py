f9311a3106a2cffbc751e5c473d5f92dcd13f846
Skywave <skywave@live.co.kr>
2015-03-26 14:54:32 +0900

    added quick book add

--- zp_library/urls.py
+++ zp_library/urls.py
@@ -4,13 +4,16 @@ from zp_library.views import *
 urlpatterns = [
     url(r'^$', template_view.MainPageView.as_view(), name='main'),
     url(r'^user/$', template_view.UserView.as_view(), name='user'),
-    url(r'^test/$', form_view.TestView.as_view(), name='test'),
     url(r'^book_list/$', template_view.BookListView.as_view(), name='book_list'),
-    url(r'^add_isbn/$', form_view.ISBNAddView.as_view(), name='add_isbn'),
     url(r'^book_detail/$', template_view.BookDetailView.as_view(), name='book_detail'),
-    url(r'^book_edit/$', form_view.BookEditView.as_view(), name='book_edit'),
-    url(r'^book_delete/$', view.BookDeleteView.as_view(), name='book_delete'),
     url(r'^parse/$', template_view.ParseView.as_view()),
+    url(r'^barcode/$', template_view.BarcodeView.as_view(), name='barcode'),
+
+    url(r'^test/$', form_view.TestView.as_view(), name='test'),
+    url(r'^add_isbn/$', form_view.ISBNAddView.as_view(), name='add_isbn'),
+    url(r'^book_edit/$', form_view.BookEditView.as_view(), name='book_edit'),
     url(r'^signup/$', form_view.SignUpView.as_view()),
-    url(r'^barcode/$', template_view.BarcodeView.as_view(), name='barcode')
+
+    url(r'^book_delete/$', view.BookDeleteView.as_view(), name='book_delete'),
+    url(r'^book_add/$', view.BookAddView.as_view(), name="book_add")
 ]
--- zp_library/views/view.py
+++ zp_library/views/view.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 
 from zp_library.api import library_search, auth
 from zp_library.models import *
+from zp_library.forms import ISBNForm
 
 
 class BookDeleteView(View):
@@ -23,4 +24,27 @@ class BookDeleteView(View):
             return HttpResponseRedirect('/book_list')
 
         return super(BookDeleteView, self).dispatch(request, *args, **kwargs)
+
+
+class BookAddView(View):
+    def dispatch(self, request, *args, **kwargs):
+        library_user = auth.get_library_user()
+        isbn = request.GET.get('isbn')
+
+        if not library_user:
+            return HttpResponseRedirect(auth.get_login_url('/'))
+
+        if not library_user.type == auth.USER_TYPE_ADMIN:
+            return HttpResponse(status=401)
+
+        if library_user.type == auth.USER_TYPE_ADMIN:
+            data = {
+                "isbn_input": isbn
+            }
+            form = ISBNForm(data)
+
+            form.action()
+            return HttpResponseRedirect('/book_detail/?isbn=' + isbn)
+
+        return super(BookAddView, self).dispatch(request, *args, **kwargs)
 
